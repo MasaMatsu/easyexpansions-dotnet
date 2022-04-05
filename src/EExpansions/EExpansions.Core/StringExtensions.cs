06e8ddf9d61dffd936059e9e16297121997f9f37
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-04-05 21:55:48 +0900

    Add StringExtensions

---
+++ src/EExpansions/EExpansions.Core/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace EExpansions.Core;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// Indicates whether the specified string is <see langword="null" /> or an empty string ("").
+    /// <para>This method is a wrapper of <see cref="string.IsNullOrEmpty(string)"/></para>
+    /// </summary>
+    /// <param name="value">The string to test.</param>
+    /// <returns>
+    /// <see langword="true" /> if the <paramref name="value" /> parameter is <see langword="null" /> or an empty string (""); otherwise, <see langword="false" />.
+    /// </returns>
+    public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
+
+    /// <summary>
+    /// Indicates whether a specified string is <see langword="null" />, empty, or consists only of white-space characters.
+    /// </summary>
+    /// <param name="value">The string to test.</param>
+    /// <returns>
+    /// <see langword="true" /> if the <paramref name="value" /> parameter is <see langword="null" /> or <see cref="string.Empty" />, or if <paramref name="value" /> consists exclusively of white-space characters.
+    /// </returns>
+    public static bool IsNullOrWhitespace(this string value) => string.IsNullOrWhiteSpace(value);
+}
