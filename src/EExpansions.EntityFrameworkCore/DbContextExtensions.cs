9aa2811687f96387ba5c24f8a1db2208d06347b2
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-04-23 03:30:12 +0900

    Add soft remove logic

--- src/EExpansions.EntityFrameworkCore/DbContextExtensions.cs
+++ src/EExpansions.EntityFrameworkCore/DbContextExtensions.cs
@@ -5,6 +5,118 @@ namespace EExpansions.EntityFrameworkCore;
 
 public static class DbContextExtensions
 {
+    #region Soft remove
+
+    /// <summary>
+    /// Enables the <see cref="IEntitySoftDeletionRecordable.IsDeleted"/> of the given entity to begin tracking
+    /// such that it will be soft deleted from the database when <see cref="DbContext.SaveChanges"/> is called.
+    /// </summary>
+    /// <typeparam name="TContext">The type of context.</typeparam>
+    /// <param name="context">The context for the method chain.</param>
+    /// <param name="entity">The entity to soft remove.</param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="context"/> or <paramref name="entity"/> are <see langword="null" />.
+    /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// <paramref name="entity"/> does not inherit <see cref="IEntitySoftDeletionRecordable"/>.
+    /// </exception>
+    public static void SoftRemove<TContext>(this TContext context, object entity)
+        where TContext : DbContext
+    {
+        _ = context ?? throw new ArgumentNullException(nameof(context));
+        _ = entity ?? throw new ArgumentNullException(nameof(entity));
+
+        if (entity is IEntitySoftDeletionRecordable deletable)
+        {
+            deletable.IsDeleted = true;
+        }
+        else
+        {
+            throw new InvalidOperationException(
+                $"The '{entity.GetType().Name}' does not inherit {nameof(IEntitySoftDeletionRecordable)}."
+            );
+        }
+    }
+
+    /// <summary>
+    /// Enables the <see cref="IEntitySoftDeletionRecordable.IsDeleted"/> of the given entity to begin tracking
+    /// such that it will be soft deleted from the database when <see cref="DbContext.SaveChanges"/> is called.
+    /// </summary>
+    /// <typeparam name="TContext">The type of context.</typeparam>
+    /// <typeparam name="TEntity">The type of entity.</typeparam>
+    /// <param name="context">The context for the method chain.</param>
+    /// <param name="entity">The entity to soft remove.</param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="context"/> or <paramref name="entity"/> are <see langword="null" />.
+    /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// <paramref name="entity"/> does not inherit <see cref="IEntitySoftDeletionRecordable"/>.
+    /// </exception>
+    public static void SoftRemove<TContext, TEntity>(this TContext context, TEntity entity)
+        where TContext : DbContext
+        where TEntity : class
+    {
+        _ = context ?? throw new ArgumentNullException(nameof(context));
+        _ = entity ?? throw new ArgumentNullException(nameof(entity));
+
+        if (entity is IEntitySoftDeletionRecordable deletable)
+        {
+            deletable.IsDeleted = true;
+        }
+        else
+        {
+            throw new InvalidOperationException(
+                $"The '{entity.GetType().Name}' does not inherit {nameof(IEntitySoftDeletionRecordable)}."
+            );
+        }
+    }
+
+    /// <summary>
+    /// Enables the <see cref="IEntitySoftDeletionRecordable.IsDeleted"/> of the given entity to begin tracking
+    /// such that it will be soft deleted from the database when <see cref="DbContext.SaveChanges"/> is called.
+    /// </summary>
+    /// <typeparam name="TContext">The type of context.</typeparam>
+    /// <param name="context">The context for the method chain.</param>
+    /// <param name="entities">The sequence of the entities to soft remove.</param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="context"/> or <paramref name="entities"/> are <see langword="null" />.
+    /// </exception>
+    public static void SoftRemoveRange<TContext>(this TContext context, IEnumerable<object> entities)
+        where TContext : DbContext
+    {
+        _ = context ?? throw new ArgumentNullException(nameof(context));
+        _ = entities ?? throw new ArgumentNullException(nameof(entities));
+
+        foreach (var entity in entities)
+        {
+            context.SoftRemove(entity);
+        }
+    }
+
+    /// <summary>
+    /// Enables the <see cref="IEntitySoftDeletionRecordable.IsDeleted"/> of the given entity to begin tracking
+    /// such that it will be soft deleted from the database when <see cref="DbContext.SaveChanges"/> is called.
+    /// </summary>
+    /// <typeparam name="TContext">The type of context.</typeparam>
+    /// <param name="context">The context for the method chain.</param>
+    /// <param name="entities">The entities to remove.</param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="context"/> or <paramref name="entities"/> are <see langword="null" />.
+    /// </exception>
+    public static void SoftRemoveRange<TContext>(this TContext context, params object[] entities)
+        where TContext : DbContext
+    {
+        _ = context ?? throw new ArgumentNullException(nameof(context));
+        _ = entities ?? throw new ArgumentNullException(nameof(entities));
+
+        foreach (var entity in entities)
+        {
+            context.SoftRemove(entity);
+        }
+    }
+
+    #endregion
+
     #region Transaction
 
     internal static async Task<TResult> InternalExecuteInTransactionAsync<TContext, TResult>(
