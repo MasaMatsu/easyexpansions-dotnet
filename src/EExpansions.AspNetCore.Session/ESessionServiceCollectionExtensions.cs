bf930991f0ef9220fc57f4de3447ba8bcc8ac104
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-05-06 20:52:28 +0900

    Add generic DistributedSessionStore

---
+++ src/EExpansions.AspNetCore.Session/ESessionServiceCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using EExpansions.AspNetCore.Session;
+using Microsoft.AspNetCore.Builder;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ESessionServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds services required for application session state.
+    /// </summary>
+    /// <typeparam name="TDistributedCache">The type of distributed cache.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <returns>
+    /// The <see cref="IServiceCollection"/> so that additional calls can be chained.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="services"/> is <see langword="null" />.
+    /// </exception>
+    public static IServiceCollection AddSession<TDistributedCache>(this IServiceCollection services)
+        where TDistributedCache : IDistributedCache
+    {
+        _ = services ?? throw new ArgumentNullException(nameof(services));
+
+        services.TryAddTransient<ISessionStore, DistributedSessionStore<TDistributedCache>>();
+        services.AddDataProtection();
+        return services;
+    }
+
+    /// <summary>
+    /// Adds services required for application session state.
+    /// </summary>
+    /// <typeparam name="TDistributedCache">The type of distributed cache.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <param name="configure">The session options to configure the middleware with.</param>
+    /// <returns>
+    /// The <see cref="IServiceCollection"/> so that additional calls can be chained.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="services"/> or <paramref name="configure"/> are <see langword="null" />.
+    /// </exception>
+    public static IServiceCollection AddSession<TDistributedCache>(
+        this IServiceCollection services,
+        Action<SessionOptions> configure
+    ) where TDistributedCache : IDistributedCache
+    {
+        _ = services ?? throw new ArgumentNullException(nameof(services));
+        _ = configure ?? throw new ArgumentNullException(nameof(configure));
+
+        services.Configure(configure);
+        services.AddSession<TDistributedCache>();
+
+        return services;
+    }
+}
