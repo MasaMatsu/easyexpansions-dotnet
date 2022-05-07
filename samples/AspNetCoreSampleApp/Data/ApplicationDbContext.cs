da5bc1ec2bff176268b3116fab6b41647b875399
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-05-07 15:13:15 +0900

    Add User

--- samples/AspNetCoreSampleApp/Data/ApplicationDbContext.cs
+++ samples/AspNetCoreSampleApp/Data/ApplicationDbContext.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EExpansions.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using AspNetCoreSampleApp.Data.Models;
+using System.Security.Claims;
 
 namespace AspNetCoreSampleApp.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : EEIdentityDbContext<User>
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    private IHttpContextAccessor HttpContextAccessor { get; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor)
         : base(options)
     {
+        HttpContextAccessor = httpContextAccessor;
+    }
+
+    protected override string? GetUserId()
+    {
+        var claim = HttpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier);
+        return Convert.ChangeType(claim, typeof(string)) as string;
     }
 }
---
+++ samples/AspNetCoreSampleApp/Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using EExpansions.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreSampleApp.Data.Models;
+
+public class User : IdentityUser, IEntityCreationRecordable<string, User>, IEntityUpdationRecordable<string, User>, IEntitySoftDeletionRecordable<string, User>
+{
+    public string? CreatedBy { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public User? Creator { get; set; }
+
+    public string? UpdatedBy { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public User? Updater { get; set; }
+    
+    public string? DeletedBy { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+    public User? Deleter { get; set; }
+}
