66583b299180d7c031c7a56aabbe6d192d90c3b7
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-05-14 16:00:12 +0900

    Add EnumExtensionsTests

---
+++ tests/EExpansions.Core.Tests/EnumExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EExpansions;
+
+public class EnumExtensionsTests
+{
+    private const string DisplayName = "TheDisplayName";
+
+    private enum VerificationEnum
+    {
+        [Display(Name = DisplayName)]
+        HaveDisplayName,
+
+        HaveNoDisplayName,
+    }
+
+    [Theory]
+    [InlineData(null, null)]
+    [InlineData(VerificationEnum.HaveDisplayName, DisplayName)]
+    [InlineData(VerificationEnum.HaveNoDisplayName, null)]
+    public void GetDisplayName(Enum enumValue, string expected)
+    {
+        if (enumValue is null)
+        {
+            Assert.Throws<ArgumentNullException>(() => enumValue!.GetDisplayName());
+            return;
+        }
+        Assert.Equal(expected, enumValue.GetDisplayName());
+    }
+}
