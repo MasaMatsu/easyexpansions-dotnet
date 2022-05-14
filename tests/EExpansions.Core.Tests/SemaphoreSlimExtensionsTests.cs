cf64a1627efcf5e88fe2ab395b9a1e6d648afea5
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-05-14 16:26:10 +0900

    Add SemaphoreSlimExtensionsTests

---
+++ tests/EExpansions.Core.Tests/SemaphoreSlimExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿namespace EExpansions;
+
+public class SemaphoreSlimExtensionsTests
+{
+    [Fact]
+    public Task ExecuteInLockAsync()
+    {
+        // TODO: Implement.
+        Assert.True(true);
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public void ExecuteInLock()
+    {
+        // TODO: Implement.
+        Assert.True(true);
+    }
+}
