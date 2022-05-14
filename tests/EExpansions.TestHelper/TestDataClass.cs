90798ec813df7ed247094954c1aa11d9e57def2a
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-05-14 14:57:36 +0900

    Add EExpansions.TestHelper

---
+++ tests/EExpansions.TestHelper/TestDataClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace EExpansions.TestHelper;
+
+public abstract class TestDataClass : IEnumerable<object?[]>
+{
+    protected IList<object?[]> TestData = new List<object?[]>();
+
+    public TestDataClass()
+    {
+        Initialize();
+    }
+
+    protected abstract void Initialize();
+
+    public IEnumerator<object?[]> GetEnumerator() => TestData.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
