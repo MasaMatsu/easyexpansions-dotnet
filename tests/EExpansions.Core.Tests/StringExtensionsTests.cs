54e2a75453232586c678fca88489301a41a93a70
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-05-14 16:33:19 +0900

    Add StringExtensionsTests

---
+++ tests/EExpansions.Core.Tests/StringExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace EExpansions;
+
+public class StringExtensionsTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("A")]
+    public void IsNullOrEmpty(string value)
+    {
+        Assert.Equal(string.IsNullOrEmpty(value), value.IsNullOrEmpty());
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("　")]
+    [InlineData("\t")]
+    [InlineData("\n")]
+    [InlineData(null)]
+    [InlineData("A")]
+    public void IsNullOrWhitespace(string value)
+    {
+        Assert.Equal(string.IsNullOrWhiteSpace(value), value.IsNullOrWhitespace());
+    }
+}
