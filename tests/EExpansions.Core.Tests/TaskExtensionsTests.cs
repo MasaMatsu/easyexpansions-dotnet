e25c9d760e4697be62cbb5515f2d088f188a73e3
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-05-14 16:34:50 +0900

    Add TaskExtensionsTests

---
+++ tests/EExpansions.Core.Tests/TaskExtensionsTests.cs
@@ -0,0 +1,108 @@
+﻿namespace EExpansions;
+
+public class TaskExtensionsTests
+{
+    [Fact]
+    public async Task WhenAll()
+    {
+        Func<IList<int>, int, Task> func = async (list, i) =>
+        {
+            await Task.Delay(i * 100);
+            list.Add(i);
+        };
+        var actual = new List<int>();
+        var expected = new List<int>();
+        var actualTasks = new List<Task>
+        {
+            func(actual, 1),
+            func(actual, 2),
+            func(actual, 3),
+            func(actual, 4),
+            func(actual, 5),
+        };
+        var expectedTasks = new List<Task>
+        {
+            func(expected, 1),
+            func(expected, 2),
+            func(expected, 3),
+            func(expected, 4),
+            func(expected, 5),
+        };
+
+        await actualTasks.WhenAll();
+        await Task.WhenAll(expectedTasks);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task WhenAll_WithResult()
+    {
+        var actual =
+            await Enumerable.Range(1, 5)
+            .Select(i => Task.FromResult(i))
+            .WhenAll();
+        var expected =
+            await Task.WhenAll(
+                Enumerable.Range(1, 5)
+                .Select(i => Task.FromResult(i))
+            );
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task WhenAllSync()
+    {
+        Func<IList<int>, int, Task> func = async (list, i) =>
+        {
+            await Task.Delay(i * 100);
+            list.Add(i);
+        };
+        var actual = new List<int>();
+        var expected = new List<int>();
+        var actualTasks = new List<Task>
+        {
+            func(actual, 1),
+            func(actual, 2),
+            func(actual, 3),
+            func(actual, 4),
+            func(actual, 5),
+        };
+        var expectedTasks = new List<Task>
+        {
+            func(expected, 1),
+            func(expected, 2),
+            func(expected, 3),
+            func(expected, 4),
+            func(expected, 5),
+        };
+
+        await actualTasks.WhenAllSync();
+        await Task.WhenAll(expectedTasks);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task WhenAllSync_WithResult()
+    {
+        var actual =
+            await Enumerable.Range(1, 5)
+            .Select(i => Task.FromResult(i))
+            .WhenAllSync();
+        var expected =
+            await Task.WhenAll(
+                Enumerable.Range(1, 5)
+                .Select(i => Task.FromResult(i))
+            );
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public Task WhenAny()
+    {
+        // TODO: Implement.
+        Assert.True(true);
+        return Task.CompletedTask;
+    }
+}
