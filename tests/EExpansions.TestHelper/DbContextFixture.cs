2a157f074fcbec2762334f24ed3dbc61965d93bc
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2022-05-16 21:20:30 +0900

    Add DbContextExtensionsTests

---
+++ tests/EExpansions.TestHelper/DbContextFixture.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EExpansions.TestHelper;
+
+public abstract class DbContextFixture<TContext> : IDisposable
+    where TContext : DbContext, new()
+{
+    public DbContextFixture()
+    {
+        using var context = CreateDbContext();
+
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        Initialize(context);
+    }
+
+    public void Dispose()
+    {
+        using var context = CreateDbContext();
+
+        context.Database.EnsureDeleted();
+    }
+
+    protected abstract void Initialize(TContext context);
+
+    public TContext CreateDbContext() => new TContext();
+
+    public IDbContextFactory<TContext> CreateDbContextFactory() => new MockDbContextFactory<TContext>();
+}
+
+public class MockDbContextFactory<TContext> : IDbContextFactory<TContext>
+    where TContext : DbContext, new()
+{
+    public TContext CreateDbContext() => new TContext();
+}
