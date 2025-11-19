0f2484f0bf3a913f66ffb29500b30d7c0f1d5d73
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2025-11-19 17:04:38 +0900

    check

--- samples/AspNetCoreSampleApp/Data/Migrations/20251119075657_dotnet10.Designer.cs
+++ samples/AspNetCoreSampleApp/Data/Migrations/20251119075657_dotnet10.Designer.cs
@@ -4,6 +4,7 @@ using AspNetCoreSampleApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AspNetCoreSampleApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251119075657_dotnet10")]
+    partial class dotnet10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
